ec9c11e006a8aa655c3735d8769322ffb9683f49
thaiantt <thaian.tt@gmail.com>
2017-11-27 18:38:57 +0100

    Added preprocessing.py and dealed with categorical columns

---
+++ Test_twisted/README.md
@@ -0,0 +1,7 @@
+# README
+
+Before launching the code, please install :
+- Twisted
+    > pip install Twisted
+- autobahn
+    > pip install autobahn
---
+++ Test_twisted/preprocessing.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+# jsonfile
+# key : row
+# values : col_1, col_2, ... col_p
+jsonfile = '{"row 1":{"col 1":"a","col 2":"b", "col3":1, "col4":1.1},' \
+           '"row 2":{"col 1":"c","col 2":"d", "col3":2, "col4":2.2}}'
+
+
+def create_df():
+    df = pd.read_json(jsonfile, orient='index')
+    return df
+
+
+def remove_categorical_var(df):
+    df_cleaned = df.select_dtypes(['number'])
+    return df_cleaned
+
+
+if __name__ == '__main__':
+    df = create_df()
+    print(df)
+
+    df_cleaned = remove_categorical_var(df)
+    print(df_cleaned)
+    print(df_cleaned.dtypes)
