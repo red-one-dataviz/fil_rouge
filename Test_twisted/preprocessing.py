b6534a329b692bea5a9112cf401bf44b7ed45ba1
thaiantt <thaian.tt@gmail.com>
2017-12-03 18:19:51 +0100

    Added small method in preprocessing

--- Test_twisted/preprocessing.py
+++ Test_twisted/preprocessing.py
@@ -1,16 +1,26 @@
 import pandas as pd
 
 
+# Create DataFrame from JSON file
 def create_df(json_str):
     df = pd.read_json(json_str, orient='records')
     return df
 
 
+# Keep numerical columns only
 def remove_categorical_var(df):
     df_cleaned = df.select_dtypes(['number'])
     return df_cleaned
 
 
+# To be improved
+# Currently : removed every row with at least one empty value
+def remove_nan(df):
+    df_cleaned = df.dropna(axis=0)
+    return df_cleaned
+
+
+# Create new JSON file from DataFrame
 def create_json(df):
     json_str = df.to_json(orient='records')
     return json_str
